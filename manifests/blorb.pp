5543c34d9f8ea3acab2f13a40e03e95a15273d16
Nikita Pekin <contact@nikitapek.in>
2014-07-15 23:36:05 -0400

    Initial cleanup

--- manifests/blorb.pp
+++ manifests/blorb.pp
@@ -1,21 +1,20 @@
-class {'base::etienne':}
+class { 'base::indiv0': }
 
-class {'console':}
+class { 'console': }
 
-class {'desktop':
-	graphics_drivers => 'catalyst',
-	games => true,
-	dpi => 102,
-	dotsync => '/media/EncData/DotSync',
-	kde_compositing => 'XRender'
+class { 'desktop':
+    graphics_drivers => 'nvidia',
+    games => true,
+    dpi => 102,
 }
 
-network_interface {$ethernet_macaddress:
-	rename => 'wired0',
-	macspoof => false, # Desktop machine, not much risk
-	ipv6_privacy => true
+network_interface { $ethernet_macaddress:
+    rename => 'wired0',
+    macspoof => false, # Desktop machine, not much risk
+    ipv6_privacy => true
 }
 
-class {'synergy::server':
-	config => template('private/synergy/blorb2.synergy.conf')
+class { 'synergy::server':
+    config => template('private/synergy/blorb2.synergy.conf'),
 }
+
