e0268e310a97cce43eb86eefdad217089233ea79
Nikita Pekin <contact@nikitapek.in>
2014-07-15 23:48:36 -0400

    Update manifests

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -4,13 +4,13 @@ class { 'console': }
 
 class { 'desktop':
     graphics_drivers => 'nvidia',
-    games => true,
-    dpi => 102,
+    games            => true,
+    dpi              => 102,
 }
 
 network_interface { $ethernet_macaddress:
-    rename => 'wired0',
-    macspoof => false, # Desktop machine, not much risk
+    rename       => 'wired0',
+    macspoof     => false, # Desktop machine, not much risk
     ipv6_privacy => true
 }
 
