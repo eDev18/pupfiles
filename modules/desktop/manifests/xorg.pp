a503e764fe4de0d8f721b9aa4816a63e70fc871f
Nikita Pekin <contact@nikitapek.in>
2014-07-18 00:38:39 -0400

    Add xorg-xsetroot package as dependency

--- modules/desktop/manifests/xorg.pp
+++ modules/desktop/manifests/xorg.pp
@@ -1,4 +1,5 @@
 class desktop::xorg {
-    package {'xorg-server':}
-    package {'xorg-xinit':}
+    package { 'xorg-server': }
+    package { 'xorg-xinit': }
+    package { 'xorg-xsetroot': } # Provides `xsetroot` command.
 }
