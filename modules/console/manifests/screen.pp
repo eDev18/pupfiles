08edd2ef9d02b750e5f6043fa65baa2ed8d6fc44
Nikita Pekin <contact@nikitapek.in>
2014-07-20 16:52:56 -0400

    Add tmux config

--- modules/console/manifests/screen.pp
+++ modules/console/manifests/screen.pp
@@ -1,4 +1,14 @@
 class console::screen {
     package {'tmux':}
     aur_package {'tmuxinator':}
+    enduser_file { '.tmux.conf':
+        ensure  => present,
+        source  => 'console/screen',
+        require => Package['tmux']
+    }
+    enduser_file { '.tmuxline':
+        ensure  => present,
+        source  => 'console/screen',
+        require => Package['tmux']
+    }
 }
