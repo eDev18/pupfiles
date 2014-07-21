52c7445cbcb6a68819718d6f73cdd3909b2ff031
Nikita Pekin <contact@nikitapek.in>
2014-07-21 01:37:08 -0400

    More work on bspwm and bar

--- modules/desktop/manifests/wm/bspwm.pp
+++ modules/desktop/manifests/wm/bspwm.pp
@@ -1,8 +1,11 @@
 class desktop::wm::bspwm {
     package {'acpi':} # Needed for bar
+    package {'bc':}
+    package {'lm_sensors':}
     aur_package {'bspwm':}
     aur_package {'bar-aint-recursive-git':}
     aur_package {'gohufont':}
+    aur_package {'stlarch_font':}
     enduser_file { '.config/bspwm':
         ensure  => directory,
         source  => 'desktop/wm/bspwm',
@@ -17,9 +20,9 @@ class desktop::wm::bspwm {
         mode    => 0744,
         require => Package['bspwm']
     }
-    enduser_file { '.config/bspwm/panel/panel':
+    enduser_file { '.config/bspwm/panel/panel_conky':
         ensure  => present,
-        content => template('desktop/wm/bspwm/.config/bspwm/panel/panel.erb'),
+        content => template('desktop/wm/bspwm/.config/bspwm/panel/panel_conky.erb'),
         mode    => 0744,
         require => Package['bspwm']
     }
