258ce81ffc2c513f031af7db173871634565761e
Nikita Pekin <contact@nikitapek.in>
2014-07-22 19:19:10 -0400

    Remove TODO

--- modules/console/manifests/shutils.pp
+++ modules/console/manifests/shutils.pp
@@ -3,5 +3,15 @@ class console::shutils {
     package {'dos2unix':}
     package {'htop':}
     package {'lftp':}
+    package { 'mlocate':
+        notify => Exec['updatedb']
+    }
     package {'ranger':}
+
+    # Update mlocate db.
+    exec { 'updatedb':
+        command     => '/usr/bin/env updatedb',
+        refreshonly => true,
+        require     => Package['mlocate']
+    }
 }
