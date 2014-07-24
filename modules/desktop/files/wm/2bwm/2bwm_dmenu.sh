74977389e92618e55123f14aba249d9a9319d443
Nikita Pekin <contact@nikitapek.in>
2014-07-23 22:06:11 -0400

    Fix dmenu

--- modules/desktop/files/wm/2bwm/2bwm_dmenu.sh
+++ modules/desktop/files/wm/2bwm/2bwm_dmenu.sh
@@ -1,2 +1,7 @@
-#!/usr/bin/sh
-dmenu_run -q -l 5 -w 250 -y 18 -p ">" -fn "Tamsyn:6" -nb "#2d2d2d" -sb "#3f3f3f" -sf "#d64937"
+#!/bin/sh
+FONT="Tamsyn:6"
+BG_COLOR="#282a2e"
+CURSOR_BG_COLOR="#3f3f3f"
+CURSOR_FG_COLOR="#d64937"
+
+dmenu_run -q -l 5 -w 250 -y 18 -p ">" -fn "$FONT" -nb "$BG_COLOR" -sb "$CURSOR_BG_COLOR" -sf "$CURSOR_FG_COLOR"
