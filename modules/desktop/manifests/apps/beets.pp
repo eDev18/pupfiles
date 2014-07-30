3fb3470ab7e8797db4ab3b2163744b530b912995
Nikita Pekin <contact@nikitapek.in>
2014-07-30 00:03:14 -0400

    Enable discogs plugin for beets

--- modules/desktop/manifests/apps/beets.pp
+++ modules/desktop/manifests/apps/beets.pp
@@ -1,8 +1,9 @@
 class desktop::apps::beets {
-    package{ 'beets': }
-    package{ 'python2-requests': }
-    package{ 'python2-mpd': }
-    package{ 'python2-flask': }
+    package { 'beets': }
+    package { 'python2-requests': }
+    package { 'python2-mpd': }
+    package { 'python2-flask': }
+    aur_package { 'python2-discogs-client': }
 
     enduser_file { '.config/beets':
         source  => 'desktop/apps/beets',
