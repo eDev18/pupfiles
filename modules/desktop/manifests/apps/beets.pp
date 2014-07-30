07169af075d86a16b09e044819e8c3b95ef0fe62
Nikita Pekin <contact@nikitapek.in>
2014-07-29 23:28:17 -0400

    Enable beets mpdstats and mpdupdate plugins

--- modules/desktop/manifests/apps/beets.pp
+++ modules/desktop/manifests/apps/beets.pp
@@ -1,6 +1,7 @@
 class desktop::apps::beets {
     package{ 'beets': }
     package{ 'python2-requests': }
+    package{ 'python2-mpd': }
 
     enduser_file { '.config/beets':
         source  => 'desktop/apps/beets',
