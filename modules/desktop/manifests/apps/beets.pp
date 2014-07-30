01002fda312c41c3da934e95cd356fe25483db70
Nikita Pekin <contact@nikitapek.in>
2014-07-29 23:29:49 -0400

    Enable beets web plugin

--- modules/desktop/manifests/apps/beets.pp
+++ modules/desktop/manifests/apps/beets.pp
@@ -2,6 +2,7 @@ class desktop::apps::beets {
     package{ 'beets': }
     package{ 'python2-requests': }
     package{ 'python2-mpd': }
+    package{ 'python2-flask': }
 
     enduser_file { '.config/beets':
         source  => 'desktop/apps/beets',
