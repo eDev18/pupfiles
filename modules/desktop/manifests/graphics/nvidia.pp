1bd679fc61fe352f7ba8e6f7be270531b0bd81e6
Nikita Pekin <contact@nikitapek.in>
2014-08-01 17:43:29 -0400

    Revert nvidia

--- modules/desktop/manifests/graphics/nvidia.pp
+++ modules/desktop/manifests/graphics/nvidia.pp
@@ -1,10 +1,25 @@
 class desktop::graphics::nvidia {
+    # package {'nvidia':}
+    aur_package { 'nvidia-dkms': }
+    package {'nvidia-utils':}
     package {'libva-vdpau-driver':}
     include base::packaging::multilib
+    package { 'lib32-nvidia-utils':
+        require => Class['base::packaging::multilib']
+    }
+    package { 'lib32-nvidia-libgl':
+        require => Class['base::packaging::multilib']
+    }
     include base::mkinitcpio
     include desktop::xorg
+    file { '/etc/X11/xorg.conf.d/20-nvidia.conf':
+        source  => 'puppet:///modules/desktop/graphics/nvidia/20-nvidia.conf',
+        require => Class['desktop::xorg'],
+    }
     file { '/etc/X11/edid-shimian.bin':
         source  => 'puppet:///modules/desktop/graphics/edid-shimian.bin',
         require => Class['desktop::xorg'],
     }
+
+    aur_package { 'nvidia-hook': }
 }
